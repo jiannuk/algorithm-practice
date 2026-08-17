b87f8c646ef4c5c016ccbed8d8b15f2d450d8802
jiannuk <jiannuk@163.com>
2026-08-17 18:23:31 +0800

    LeetCode
    中序：左根右

--- week2/94_preorderp.py
+++ week2/94_preorderp.py
@@ -1,1 +1,12 @@
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
 
+        def dfs(node):  
+            if not node:
+                return
+            dfs(node.left)
+            result.append(node.val)
+            dfs(node.right)
+        dfs(root)
+        return result
