35e0673a75f0f3004152b449b8d48697047ddeb5
jiannuk <jiannuk@163.com>
2026-08-17 18:21:40 +0800

    LeetCode144

--- week2/144_preorder.py
+++ week2/144_preorder.py
@@ -1,1 +1,12 @@
+class Solution:   
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
 
+        def dfs(node):   # dfs 深度优先搜索 语法：
+            if not node:
+                return
+            result.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return resul
